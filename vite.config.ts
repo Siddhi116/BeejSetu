1162696f5c54169022c03159e94b02ac2c898f36
siddhisawant <sawantsiddhi116@gmail.com>
2025-10-05 21:41:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,3 +8,14 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/BeejSetu/', // 👈 your GitHub repo name here
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+})
