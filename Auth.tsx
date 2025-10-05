1e2d54743fb8ef75209c793bd1d3e6fc44989e21
siddhisawant <sawantsiddhi116@gmail.com>
2025-10-05 20:50:13 +0530

    Update Auth.tsx

--- Auth.tsx
+++ Auth.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Sprout, Home } from 'lucide-react';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../AuthContext';
 
 interface AuthProps {
   onNavigate: (page: string) => void;
